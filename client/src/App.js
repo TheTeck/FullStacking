5262e0ce3c8a5ab9473248402d74511a760d0b48
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-04 23:52:17 -0700

    Change units to imperial and decypher and use the values for sunrise and sunset

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,17 @@ import './App.css';
 function App() {
 
   const [apiRes, setApiRes] = useState('');
+  const [sunrise, setSunrise] = useState('Sunrise: unknown');
+  const [sunset, setSunset] = useState('Sunset: unknown');
 
   function callAPI () {
       fetch("http://localhost:9000/testAPI")
       .then(res => res.json())
-      .then(res => setApiRes(res.name + ', ' + res.sys.country + '  ' + res.main.temp + "\u00B0"))
+      .then(res => {
+        setApiRes(res.name + ', ' + res.sys.country + '  ' + res.main.temp + "\u00B0");
+        setSunrise('Sunrise: ' + new Date(res.sys.sunrise * 1000).toLocaleString());
+        setSunset('Sunset: ' + new Date(res.sys.sunset * 1000).toLocaleString());
+      })
       .catch(err => err);
   }
 
@@ -24,6 +30,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {apiRes}
+        </p>
+        <p>
+          {sunrise}
+        </p>
+        <p>
+          {sunset}
         </p>
       </header>
     </div>
